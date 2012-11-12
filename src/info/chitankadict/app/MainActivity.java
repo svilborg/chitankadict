aa6f96baa75da4adeb70d4b6d56c1e55e3d285de
Svilborg <svilborg@gmail.com>
2012-11-13 00:10:39 +0200

    SearchButton Catched

--- src/info/chitankadict/app/MainActivity.java
+++ src/info/chitankadict/app/MainActivity.java
@@ -25,13 +25,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.TextView; 
 
 public class MainActivity extends SherlockActivity {
 
@@ -124,6 +126,21 @@ public class MainActivity extends SherlockActivity {
 				new DownloadImageTask(searchUrl, progress).execute();
 			}
 
+		});
+		
+		searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {		
+		    @Override
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+					cleanWord();
+
+					String searchUrl = buildSearchUrl(searchText);
+
+					new DownloadImageTask(searchUrl, progress).execute();
+		            return true;
+		        }
+		        return false;
+		    }
 		});
 
 		if (savedInstanceState == null) {
