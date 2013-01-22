1006d80bf448a686030193a9ce1ef6926c4e18e6
Svilborg <svilborg@gmail.com>
2013-01-22 10:48:02 +0200

    Optimizations

--- src/info/chitankadict/parser/JsoupWordParser.java
+++ src/info/chitankadict/parser/JsoupWordParser.java
@@ -32,24 +32,18 @@ public class JsoupWordParser {
 	 * @throws IOException
 	 */
 	public Word parse() throws IOException {
-		final Word currentWord = new Word();
+		final Word word = new Word();
 
 		Document doc = Jsoup.connect(feedUrl.toString()).get();
 
 		// Extract Word itself
-		Elements nameBox = doc.select("h1");
-
-		currentWord.setName(nameBox.text());
+		word.setName(doc.select("h1").text());
 
 		// Extract Title
-		Elements titleBox = doc.select("h2 span");
-
-		currentWord.setTitle(titleBox.text());
+		word.setTitle(doc.select("h2 span").text());
 
 		// Extract Meaning
-		Elements meaningBox = doc.select("div.meaning .data");
-
-		currentWord.setMeaning(meaningBox.html().toString());
+		word.setMeaning(doc.select("div.meaning .data").html().toString());
 
 		// Extract Synonyms
 		Elements synonymsBox = doc.select("div.synonyms ul li a");
@@ -57,25 +51,25 @@ public class JsoupWordParser {
 		for (Iterator<Element> iterator = synonymsBox.iterator(); iterator.hasNext();) {
 			Element element = (Element) iterator.next();
 
-			currentWord.addSynonym(element.text().toString());
+			word.addSynonym(element.text().toString());
 		}
 
 		// Extract Possible Misspellings
 		Elements misspellsBox = doc.select("div.incorrect-forms li");
 
-		currentWord.setMisspells(misspellsBox.html().toString());
+		word.setMisspells(misspellsBox.html().toString());
 
 		// The word is Misspelled
 		Elements isMisspelled = doc.select("span.incorrect");
 
 		if (!("").equals(isMisspelled.text())) {
-			currentWord.setError(Word.WORD_MISSPELLED);
+			word.setError(Word.WORD_MISSPELLED);
 
 			Elements correctText = doc.select("div#content p a");
 
-			currentWord.setCorrect(correctText.html());
+			word.setCorrect(correctText.html());
 		}
 
-		return currentWord;
+		return word;
 	}
 }
